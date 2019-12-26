b54295ccaf46e2eebe481a37fe5990b6833c22c8
Worell97 <46696558+Worell97@users.noreply.github.com>
2019-12-26 17:19:48 -0300

    Add files

---
+++ 2 - média/uMaquina.pas
@@ -0,0 +1,40 @@
+﻿unit uMaquina;
+
+interface
+
+uses
+  uIMaquina, Classes, uTroco, System.Generics.Collections, System.Math;
+
+type
+  TMaquinaDinheiro = class(TInterfacedObject, IMaquina)
+  public
+    function MontarTroco(aTroco: Double): TList;
+  end;
+
+implementation
+
+function TMaquinaDinheiro.MontarTroco(aTroco: Double): TList;
+var
+  LTroco : TTroco;
+  LListaTroco : TList;
+  LCedula : TCedula;
+  LQuantidade : Integer;
+  Troco : Double;
+begin
+  LListaTroco := TList.Create;
+  Troco := RoundTo(aTroco, -2); 
+  for LCedula := Low(CValorCedula) to  High(CValorCedula) do
+  begin           
+  	LQuantidade := 0;
+    while Troco >= CValorCedula[LCedula] do
+    begin
+    	LQuantidade := LQuantidade + 1; 
+      Troco := Troco - CValorCedula[LCedula];   	
+    end;
+  	LTroco := TTroco.Create(LCedula, LQuantidade);  
+    LListaTroco.Add(LTroco);  
+  end;
+  Result := LListaTroco;
+end;
+end.
+
